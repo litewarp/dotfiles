bd44e83bcb653c65d198d2398681c6c8a347cbb5
Nick Sethi <nicholas.sethi@gmail.com>
2019-11-26 16:37:09 -0800

    deleted vscode

--- .vscode/extensions/gamunu.vscode-yarn-1.6.0/out/output.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const vscode_1 = require("vscode");
-const outputChannel = vscode_1.window.createOutputChannel('yarn');
-exports.outputChannel = outputChannel;
-//# sourceMappingURL=output.js.map
