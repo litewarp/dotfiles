d51d35e2547dc5183ba7aec6ab348630b801c9ab
Nick Sethi <nicholas.sethi@gmail.com>
2021-11-12 17:42:25 -0800

    autoformat

--- nvim/lua/plugins/formatter.lua
+++ nvim/lua/plugins/formatter.lua
@@ -1,39 +1,62 @@
+
 require('formatter').setup({
+
   filetype = {
     javascript = {
       function()
         return {
-          exe = 'prettier',
-          args = {
-            '--stdin-filepath',
-            vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)),
-          },
-          stdin = true,
+          exe = "prettier",
+          args = {"--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))},
+          stdin = true
         }
-      end,
+      end
     },
-    lua = {
+    typescript = { 
       function()
         return {
-          exe = 'stylua',
-          args = {
-            '--config-path',
-            '~/.config/nvim/.stylua.toml',
-            '-',
-          },
-          stdin = true,
-        }
-      end,
+          exe = "prettier",
+          args = {"--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))},
+          stdin = true
+      }
+      end
     },
-  },
+    html = { 
+      function()
+        return {
+        exe = "prettier",
+        args = {"--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))},
+        stdin = true
+      }
+      end
+    },
+    css = { 
+      function()
+        return {
+          exe = "prettier",
+          args = {"--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))},
+          stdin = true
+        }
+      end
+    },
+    lua = { 
+      function()
+        return {
+          exe = "stylua",
+          args = {
+            "--config-path",
+            "~/.config/nvim/.stylua.toml",
+            "-"
+          },
+          stdin = true
+        }
+      end
+    },
+  }
 })
 
-vim.api.nvim_exec(
-  [[
-  augroup FormatAutogroup
-    autocmd!
-    autocmd BufWritePost *.js,*.jsx,*.ts,*.tsx,*.rs,*.lua FormatWrite
-  augroup END
-]],
-  true
-)
+vim.api.nvim_exec([[
+augroup FormatAutogroup
+  autocmd!
+  autocmd BufWritePost *.js,*.ts,*.jsx,*.tsx,*.rs,*.lua FormatWrite
+augroup END
+]], true)
