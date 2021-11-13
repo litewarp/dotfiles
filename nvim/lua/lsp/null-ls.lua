ff385d88e209c05deb5d2c6d781a80ef63119019
Nick Sethi <nicholas.sethi@gmail.com>
2021-11-13 13:16:32 -0800

    use null-ls for formatting

---
+++ nvim/lua/lsp/null-ls.lua
@@ -0,0 +1,16 @@
+
+require('null-ls').config({
+  sources = {
+    require('null-ls').builtins.formatting.prettierd,
+    require('null-ls').builtins.formatting.stylua,
+    require('null-ls').builtins.code_actions.gitsigns,
+  }
+})
+
+require('lspconfig')['null-ls'].setup({ 
+  on_attach = function(client)
+    if client.resolved_capabilities.document_formatting then
+      vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+    end
+  end
+})
