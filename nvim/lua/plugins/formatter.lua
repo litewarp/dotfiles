ff016f1f472739216d1db7e01e68fb94288a74fb
Nick Sethi <nicholas.sethi@gmail.com>
2021-11-12 21:12:35 -0800

    solve lspconfig issue

--- nvim/lua/plugins/formatter.lua
+++ nvim/lua/plugins/formatter.lua
@@ -1,62 +1,88 @@
-
 require('formatter').setup({
 
   filetype = {
     javascript = {
       function()
         return {
-          exe = "prettier",
-          args = {"--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))},
-          stdin = true
-        }
-      end
-    },
-    typescript = { 
-      function()
-        return {
-          exe = "prettier",
-          args = {"--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))},
-          stdin = true
-      }
-      end
-    },
-    html = { 
-      function()
-        return {
-        exe = "prettier",
-        args = {"--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))},
-        stdin = true
-      }
-      end
-    },
-    css = { 
-      function()
-        return {
-          exe = "prettier",
-          args = {"--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))},
-          stdin = true
-        }
-      end
-    },
-    lua = { 
-      function()
-        return {
-          exe = "stylua",
+          exe = 'prettier',
           args = {
-            "--config-path",
-            "~/.config/nvim/.stylua.toml",
-            "-"
+            '--stdin-filepath',
+            vim.api.nvim_buf_get_name(0),
           },
-          stdin = true
+          stdin = true,
         }
-      end
+      end,
     },
-  }
+    typescript = {
+      function()
+        return {
+          exe = 'prettier',
+          args = {
+            '--stdin-filepath',
+            vim.api.nvim_buf_get_name(0),
+          },
+          stdin = true,
+        }
+      end,
+    },
+    typescriptreact = {
+      function()
+        return {
+          exe = 'prettier',
+          args = {
+            '--stdin-filepath',
+            vim.api.nvim_buf_get_name(0),
+          },
+          stdin = true,
+        }
+      end,
+    },
+    html = {
+      function()
+        return {
+          exe = 'prettier',
+          args = {
+            '--stdin-filepath',
+            vim.api.nvim_buf_get_name(0),
+          },
+          stdin = true,
+        }
+      end,
+    },
+    css = {
+      function()
+        return {
+          exe = 'prettier',
+          args = {
+            '--stdin-filepath',
+            vim.api.nvim_buf_get_name(0),
+          },
+          stdin = true,
+        }
+      end,
+    },
+    lua = {
+      function()
+        return {
+          exe = 'stylua',
+          args = {
+            '--config-path',
+            '~/.config/nvim/.stylua.toml',
+            '-',
+          },
+          stdin = true,
+        }
+      end,
+    },
+  },
 })
 
-vim.api.nvim_exec([[
+vim.api.nvim_exec(
+  [[
 augroup FormatAutogroup
   autocmd!
-  autocmd BufWritePost *.js,*.ts,*.jsx,*.tsx,*.rs,*.lua FormatWrite
+  autocmd BufWritePost *.js,*.ts,*.jsx,*.tsx,*.rs,*.lua,*.html,*.css FormatWrite
 augroup END
-]], true)
+]],
+  true
+)
