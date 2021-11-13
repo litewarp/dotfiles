b9b19f27ef650efda296d4d3a0c26d7917d1281b
Nick Sethi <nicholas.sethi@gmail.com>
2021-11-13 13:29:44 -0800

    run formatters in seq

--- nvim/lua/lsp/null-ls.lua
+++ nvim/lua/lsp/null-ls.lua
@@ -1,16 +1,15 @@
-
 require('null-ls').config({
   sources = {
     require('null-ls').builtins.formatting.prettierd,
     require('null-ls').builtins.formatting.stylua,
     require('null-ls').builtins.code_actions.gitsigns,
-  }
+  },
 })
 
-require('lspconfig')['null-ls'].setup({ 
+require('lspconfig')['null-ls'].setup({
   on_attach = function(client)
     if client.resolved_capabilities.document_formatting then
-      vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+      vim.cmd('autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()')
     end
-  end
+  end,
 })
