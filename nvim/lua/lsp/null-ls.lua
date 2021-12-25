c5c8c07e8e91c8f7a93b8868539dedd5a86cf89d
Nick Sethi <nicholas.sethi@gmail.com>
2021-12-25 13:15:03 -0800

    fix nvim tree keybindings

--- nvim/lua/lsp/null-ls.lua
+++ nvim/lua/lsp/null-ls.lua
@@ -1,12 +1,9 @@
-require('null-ls').config({
+require('null-ls').setup({
   sources = {
-    require('null-ls').builtins.formatting.prettierd,
+    require('null-ls').builtins.formatting.prettier,
     require('null-ls').builtins.formatting.stylua,
     require('null-ls').builtins.code_actions.gitsigns,
   },
-})
-
-require('lspconfig')['null-ls'].setup({
   on_attach = function(client)
     if client.resolved_capabilities.document_formatting then
       vim.cmd('autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()')
--- nvim/lua/plugins/tree.lua
+++ nvim/lua/plugins/tree.lua
@@ -1,5 +1,7 @@
 vim.g.nvim_tree_respect_buf_cwd = 1
 
+local tree_cb = require('nvim-tree.config').nvim_tree_callback
+
 require('nvim-tree').setup({
   -- disables netrw completely
   disable_netrw = false,
@@ -58,54 +60,12 @@ require('nvim-tree').setup({
       -- if true, it will only use your list to set the mappings
       custom_only = false,
       -- list of mappings to set on the tree manually
-      list = {},
+      list = {
+        { key = 'C', cb = tree_cb('cd') },
+      },
     },
   },
 })
 
--- local tree_cb = require('nvim-tree.config').nvim_tree_callback
--- vim.g.nvim_tree_bindings = {
---   { key = 'C', cb = tree_cb('cd') },
--- }
-
---
---[[ local tree_cb = require'nvim-tree.config'.nvim_tree_callback
-  vim.g.nvim_tree_bindings = {
-    { key = {"<CR>", "o", "<2-LeftMouse>", "l"}, cb = tree_cb("edit") },
-    { key = {"<2-RightMouse>", "<C-]>"},    cb = tree_cb("cd") },
-    { key = "<C-v>",                        cb = tree_cb("vsplit") },
-    { key = "<C-x>",                        cb = tree_cb("split") },
-    { key = "<C-t>",                        cb = tree_cb("tabnew") },
-    { key = "<",                            cb = tree_cb("prev_sibling") },
-    { key = ">",                            cb = tree_cb("next_sibling") },
-    { key = "P",                            cb = tree_cb("parent_node") },
-    { key = {"<BS>", "<S-CR>", "h"},                         cb = tree_cb("close_node") },
-    { key = "<Tab>",                        cb = tree_cb("preview") },
-    { key = "K",                            cb = tree_cb("first_sibling") },
-    { key = "J",                            cb = tree_cb("last_sibling") },
-    { key = "I",                            cb = tree_cb("toggle_ignored") },
-    { key = "H",                            cb = tree_cb("toggle_dotfiles") },
-    { key = "R",                            cb = tree_cb("refresh") },
-    { key = "a",                            cb = tree_cb("create") },
-    { key = "d",                            cb = tree_cb("remove") },
-    { key = "r",                            cb = tree_cb("rename") },
-    { key = "<C-r>",                        cb = tree_cb("full_rename") },
-    { key = "x",                            cb = tree_cb("cut") },
-    { key = {"c", "yy"},                            cb = tree_cb("copy") },
-    { key = "p",                            cb = tree_cb("paste") },
-    { key = "y",                            cb = tree_cb("copy_name") },
-    { key = "Y",                            cb = tree_cb("copy_path") },
-    { key = "gy",                           cb = tree_cb("copy_absolute_path") },
-    { key = "[c",                           cb = tree_cb("prev_git_item") },
-    { key = "]c",                           cb = tree_cb("next_git_item") },
-    { key = "-",                            cb = tree_cb("dir_up") },
-    { key = "q",                            cb = tree_cb("close") },
-    { key = "g?",                           cb = tree_cb("toggle_help") },
-} ]]
-
-vim.api.nvim_set_keymap(
-  'n',
-  '<Bar>',
-  "<cmd>lua require'nvim-tree'.toggle()<CR>",
-  { noremap = true, silent = true }
-)
+vim.api.nvim_set_keymap('n', '\\', '<cmd>:NvimTreeToggle<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Bar>', '<cmd>:NvimTreeFindFile<CR>', { noremap = true, silent = true })
